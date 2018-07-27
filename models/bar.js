e300b69bbda0fa9c6ddc4d5c89e86894fa366167
MilcahHalili <halilicenteno@gmail.com>
2018-07-27 16:30:27 -0700

    remove bar function

--- models/bar.js
+++ models/bar.js
@@ -15,4 +15,18 @@ var barSchema = new Schema({
     timestamps: true
 });
 
+barSchema.post('remove', function(barDoc) {
+    // obtain a reference to the Beer model
+    var Beer = this.model('Beer');
+    // find all beer docs that have this bar
+    Beer.find({bars: barDoc._id}, function(err, beers) {
+        beers.forEach(function(beerDoc) {
+            // handy remove method on mongoose arrays
+            beerDoc.bars.remove(barDoc._id);
+            // no need to wait for async to finish, thus no callback
+            beerDoc.save();
+        });
+    }); 
+});
+
 module.exports = mongoose.model('Bar', barSchema);
